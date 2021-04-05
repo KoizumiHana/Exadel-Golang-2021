c9be5d4d7f34fd862bf573c08ee7754fae8097fa
Andrei Mayorau <amayorau@exadel.com>
2021-04-05 22:04:35 +0300

    Added numbers support to third task accordingly to task

--- task3/string/string.go
+++ task3/string/string.go
@@ -11,7 +11,7 @@ func GroupByOccurrences(stringToGroup string) string {
 	words := strings.FieldsFunc(
 		stringToGroup,
 		func(r rune) bool {
-			return !unicode.IsLetter(r)
+			return !unicode.IsLetter(r) && !unicode.IsNumber(r)
 		})
 
 	type oi struct {
--- task3/string/string_test.go
+++ task3/string/string_test.go
@@ -12,17 +12,17 @@ func TestGroupByOccurrences(t *testing.T) {
 		{
 			"grouping of example string",
 			"one, two - this 2, three one two, lot of words: one",
-			"one(3) two(2) this(1) three(1) lot(1) of(1) words(1)",
+			"one(3) two(2) this(1) 2(1) three(1) lot(1) of(1) words(1)",
 		},
 		{
 			"grouping of string without multiple occurrences of same words",
 			"one, two - this 2, three, lot of words:",
-			"one(1) two(1) this(1) three(1) lot(1) of(1) words(1)",
+			"one(1) two(1) this(1) 2(1) three(1) lot(1) of(1) words(1)",
 		},
 		{
 			"grouping of example string with non consecutive indexes of words",
 			"one, two - this 2, three one two, lot of words: one! words:",
-			"one(3) two(2) words(2) this(1) three(1) lot(1) of(1)",
+			"one(3) two(2) words(2) this(1) 2(1) three(1) lot(1) of(1)",
 		},
 	}
 	for _, tt := range tests {
